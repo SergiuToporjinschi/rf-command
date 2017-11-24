a087a89044cfd395762e8db12c097feadc6079b9
Toporjinschi <sergiu.toporjinschi@gmail.com>
2017-11-24 09:44:16 +0200

    Initial commit

---
+++ src/rf-command.js
@@ -0,0 +1,23 @@
+var rf = require('./rf/rf');
+module.exports = function(RED) {
+    function LowerCaseNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.on('input', function(msg) {
+            //msg.payload = "test";
+            console.log(config.gpio);
+            //rf.setGPIO(config.gpio);
+            node.send(null);
+        });
+    }
+    RED.nodes.registerType("rf-command",LowerCaseNode);
+};
+
+
+//var gpio = require('rpi-gpio');
+//gpio.setup(7, gpio.DIR_IN, readInput);
+//function readInput() {
+//    gpio.read(7, function(err, value) {
+//        console.log('The value is ' + value);
+//    });
+//}
