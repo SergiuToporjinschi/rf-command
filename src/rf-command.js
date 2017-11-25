a4a291bc94d2646491963e5243bd546a9319b7a0
Sergiu Toporjinschi <Sergiu.Toporjinschi@gmail.com>
2017-11-25 07:43:06 +0200

    Buttons in toolbar have functions

--- src/rf-command.js
+++ src/rf-command.js
@@ -1,18 +1,76 @@
 var rf = require('./rf/rf');
-module.exports = function(RED) {
+var gpio = require('rpi-gpio');
+var async = require('async');
+
+module.exports = function (RED) {
+
+    function write(cod) {
+        [
+                function (callback) {
+                    delayedWrite(7, true, callback);
+                },
+                function (callback) {
+                    delayedWrite(7, true, callback);
+                },
+                function (callback) {
+                    delayedWrite(7, true, callback);
+                },
+                function (callback) {
+                    delayedWrite(7, false, callback);
+                },
+                function (callback) {
+                    delayedWrite(7, false, callback);
+                },
+                function (callback) {
+                    delayedWrite(7, false, callback);
+                }
+        ],
+        
+        
+        async.series(cod, function (err, results) {
+            console.log('Writes complete, pause then unexport pins');
+            setTimeout(function () {
+                gpio.destroy(function () {
+                    console.log('Closed pins, now exit');
+                });
+            }, 500);
+        });
+    }
+
+    function delayedWrite(pin, value, ms, callback) {
+        setTimeout(function () {
+            gpio.write(pin, value, callback);
+        }, ms);
+    }
+
+    function getInit(lowTime, highTime) {
+        return [
+            function (callback) {
+                delayedWrite(7, true, highTime, callback);
+            },
+            function (callback) {
+                delayedWrite(7, false, lowTime, callback);
+            }
+        ];
+    }
     function LowerCaseNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-        node.on('input', function(msg) {
+        node.on('input', function (msg) {
+
             //msg.payload = "test";
             console.log(config);
-            console.log(config.gpio);
+            console.log(msg);
             console.log(config.initSeq);
-            rf.setGPIO(config.gpio);
+            var cod = [];
+            if (config.initSeq) {
+                cod.push(getInit(config.initTimeLow, config.initTimeHigh));
+            }
+            //rf.setGPIO(config.gpio);
             node.send(null);
         });
     }
-    RED.nodes.registerType("rf-command",LowerCaseNode);
+    RED.nodes.registerType("rf-command", LowerCaseNode);
 };
 
 
